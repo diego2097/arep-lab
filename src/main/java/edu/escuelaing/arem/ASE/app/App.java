bd100686f52ed44fa1dc84f26f76c6e5186ea3b0
diego2097 <ye.yo14@hotmail.com>
2019-08-13 20:40:37 -0500

    cambios src

--- src/main/java/edu/escuelaing/arem/ASE/app/App.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/App.java
@@ -1,20 +1,17 @@
 package edu.escuelaing.arem.ASE.app;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+public class App {
 
-public class App 
-{
-    public static void main( String[] args ) throws IOException 
-    {
-        /*calculo("C:/Users/USUARIO/Documents/lab/prueba.txt");
-        calculo("C:/Users/USUARIO/Documents/lab/prueba2.txt");*/
+    public static void main( String[] args ) throws IOException   {
     }
+    
     
      /**
      * Este metodo se encarga de leer los datos y realizar las operaciones respectivas para calcular la media y las desviacion
@@ -29,6 +26,7 @@ public class App
         BufferedReader b = new BufferedReader(f);
         int n=0;
         float suma=0;
+		
         LinkedList<Double> lista = new LinkedList<Double>();
         while((cadena = b.readLine())!=null) {      
             n++;
@@ -37,15 +35,13 @@ public class App
             suma+=number;
         }
         b.close();  
+		
         double media=suma/n;
-        /*System.out.println("la media es: "+media);*/
-        
         double numerador=0;
         for (int i=0; i<lista.size();i++){
             numerador+=Math.pow(lista.get(i)-media,2);
         }
         double desv=Math.sqrt(numerador/(lista.size()-1));
-        /*System.out.println("la desviacion estandar es: "+desv);*/
         ArrayList<Double> rta  = new ArrayList<Double>();
         rta.add(media);
         rta.add(desv);
